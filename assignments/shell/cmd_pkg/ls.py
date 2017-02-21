6bd81a58a8a6e4aedee9d1cc445dcf8da5e8f7c0
Lavanya <lavanyamengaraboina@gmail.com>
2017-02-21 15:42:53 -0600

    Delete ls.py

--- assignments/shell/cmd_pkg/ls.py
+++
@@ -1,6 +0,0 @@
-def ls():
-        for dirname, dirnames, filenames in os.walk('.'):
-            for subdirname in dirnames:
-                print(os.path.join(dirname,subdirname))
-        for filename in filenames:
-            print(os.path.join(dirname, filename))
